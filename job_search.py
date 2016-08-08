bbf0e367e712ecd91b3f0e0abc84845879530a4c
Antony Semonella <asemonella@gmail.com>
2016-08-08 13:27:07 +0100

    Merge feeds

--- job_search.py
+++ job_search.py
@@ -9,3 +9,14 @@ class JobSearch(object):
             {'name': endpoint['name'], 'url': endpoint['url'].format(**args)}
             for endpoint in json.load(open(endpoints))
         ]
+        self.feeds = []
+        self.merged_feeds = []
+
+    def merge_feeds(self):
+        self.merged_feeds = [
+            entry for feed in self.feeds for entry in feed['entries']
+        ]
+        # Sort by latest date first
+        self.merged_feeds.sort(
+            key=lambda feed: feed['published_parsed'], reverse=True
+        )
--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,8 @@
+import pickle
 import unittest
+
+import feedparser
+from mock import patch
 
 from job_search import JobSearch
 
@@ -31,3 +35,25 @@ class TestFeedEndpoints(unittest.TestCase):
 
     def test_endpoints_loaded(self):
         self.assertListEqual(self.endpoints, self.job_search.endpoints)
+
+
+class TestFeedParsing(unittest.TestCase):
+
+    def setUp(self):
+        # Sample feed data
+        self.feed_1 = feedparser.parse('tests/example_feed_1.xml')
+        self.feed_2 = feedparser.parse('tests/example_feed_2.xml')
+        self.endpoint_args = {
+            'search_term': 'python',
+            'location': 'leeds',
+            'distance': 5
+        }
+        self.job_search = JobSearch('tests/endpoints.json', self.endpoint_args)
+
+    def testMergeFeeds(self):
+        self.job_search.feeds = [self.feed_1, self.feed_2]
+        self.job_search.merge_feeds()
+        self.assertListEqual(
+            pickle.load(open('tests/merged_feeds.pkl')),
+            self.job_search.merged_feeds
+        )
